36bb8efab0ba714296bc0741d1d6f48cdddfedf4
jesus m. rodriguez <jesusr@redhat.com>
2015-05-19 13:59:30 -0400

    skip content is configurable

--- fusor/deploy.sh
+++ fusor/deploy.sh
@@ -1,1 +1,6 @@
-curl -k -s -u admin:changeme -H "Accept: application/json" -H "Content-Type: application/json" -X PUT https://sat61fusor.example.com:443/fusor/api/v2/deployments/$1/deploy?skip_content=true | python -mjson.tool
+if [ "$1" = "" ]; then
+  SKIP_CONTENT=false
+else
+  SKIP_CONTENT=true
+fi
+curl -k -s -u admin:changeme -H "Accept: application/json" -H "Content-Type: application/json" -X PUT https://sat61fusor.example.com:443/fusor/api/v2/deployments/$1/deploy?skip_content=$SKIP_CONTENT | python -mjson.tool
