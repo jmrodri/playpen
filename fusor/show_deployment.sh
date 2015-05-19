39e18825eb3a52a174571bf12997d34a871a3aec
jesus m. rodriguez <jesusr@redhat.com>
2015-05-19 13:48:07 -0400

    use a common configuration for IP and PORT
    The scripts can now be configured using the config file simply set the ip
    address of your satellite and a port if needed (port defaults to 443). IPADDRE
    defaults to empty.

--- fusor/show_deployment.sh
+++ fusor/show_deployment.sh
@@ -1,1 +1,6 @@
-curl -k -s -u admin:changeme -H "Accept: application/json" -X GET https://192.168.121.144:443/fusor/api/v21/deployments/$1 | python -mjson.tool
+source config
+if [ "$IPADDR" = "" ]; then
+    echo "Supply an IPADDR for your satellite in config"
+    exit
+fi
+curl -k -s -u admin:changeme -H "Accept: application/json" -X GET https://$IPADDR:$PORT/fusor/api/v21/deployments/$1 | python -mjson.tool
