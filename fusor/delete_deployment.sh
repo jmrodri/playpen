eb2cf7e6ea4141fb28efaaac249ab20a24a647b3
jesus m. rodriguez <jesusr@redhat.com>
2015-05-19 13:56:28 -0400

    use sat61fusor.example.com instead of ipaddr

--- fusor/delete_deployment.sh
+++ fusor/delete_deployment.sh
@@ -1,1 +1,1 @@
-curl -k -s -u admin:changeme -H "Accept: application/json" -X DELETE https://192.168.121.144:443/fusor/api/v2/deployments/$1 | python -mjson.tool
+curl -k -s -u admin:changeme -H "Accept: application/json" -X DELETE https://sat61fusor.example.com:443/fusor/api/v2/deployments/$1 | python -mjson.tool
--- fusor/show_deployment.sh
+++ fusor/show_deployment.sh
@@ -1,1 +1,1 @@
-curl -k -s -u admin:changeme -H "Accept: application/json" -X GET https://192.168.121.144:443/fusor/api/v21/deployments/$1 | python -mjson.tool
+curl -k -s -u admin:changeme -H "Accept: application/json" -X GET https://sat61fusor.example.com:443/fusor/api/v21/deployments/$1 | python -mjson.tool
