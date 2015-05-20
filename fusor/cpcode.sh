abc6fd649c8361ba49ef1272af5ffa4e9d6730bf
jesus m. rodriguez <jesusr@redhat.com>
2015-05-20 13:25:18 -0400

    copy over deploy.rb too

--- fusor/cpcode.sh
+++ fusor/cpcode.sh
@@ -2,5 +2,6 @@ sudo cp /home/vagrant/fusor/server/app/lib/actions/fusor/deployment/deploy_cloud
 sudo mkdir -p /opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1/app/lib/utils/fusor
 sudo cp /home/vagrant/fusor/server/app/lib/utils/fusor/ssh_connection.rb /opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1/app/lib/utils/fusor/ssh_connection.rb
 sudo cp /home/vagrant/fusor/server/app/lib/utils/fusor/command_utils.rb /opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1/app/lib/utils/fusor/command_utils.rb
+sudo cp /home/vagrant/fusor/server/app/lib/actions/fusor/deploy.rb /opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1/app/lib/actions/fusor/deploy.rb
 #sudo cp /home/vagrant/fusor/server/app/lib/actions/fusor/deployment/host_converter.rb /opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1/app/lib/actions/fusor/deployment/host_converter.rb
 #sudo rm -f /opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1/app/lib/actions/fusor/deployment/host_converter.rb
