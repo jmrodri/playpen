3b0cb4608610dfa89bd46e757f20062f16b15db1
jesus m. rodriguez <jesusr@redhat.com>
2015-06-08 14:47:50 -0400

    make utils/cloud_forms

--- fusor/cpcode.sh
+++ fusor/cpcode.sh
@@ -3,6 +3,8 @@ FUSOR_APP_DIR=/opt/rh/ruby193/root/usr/share/gems/gems/fusor_server-0.0.1
 
 sudo rsync -avz $FUSOR_GIT_DIR/server/app/lib/actions/fusor/deploy.rb $FUSOR_APP_DIR/app/lib/actions/fusor/deploy.rb
 sudo rsync -avz $FUSOR_GIT_DIR/server/app/lib/actions/fusor/deployment/deploy_cloud_forms.rb $FUSOR_APP_DIR/app/lib/actions/fusor/deployment/deploy_cloud_forms.rb
+
+mkdir -p $FUSOR_APP_DIR/app/lib/utils/cloud_forms
 sudo rsync -avz $FUSOR_GIT_DIR/server/app/lib/utils/cloud_forms/provider.rb $FUSOR_APP_DIR/app/lib/utils/cloud_forms/provider.rb
 
 mkdir -p $FUSOR_APP_DIR/app/lib/utils/fusor/
